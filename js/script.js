80cb43db5ed91c3aa7409ca77a44280a08392445
Luca <memeoluca6@gmail.com>
2021-03-03 14:57:52 +0100

    created user, sorted list and printed user position in it

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni�
 stampa la lista ordinata alfabeticamente
 scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova */
 
+var surname_list = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 // ask user's surname
+var surname_user = "Malpiedi";
+surname_list.push(surname_user);
+// sort list
+surname_list.sort();
+// print list
+console.log(surname_list);
+// print user position
+console.log(surname_list.indexOf(surname_user));
