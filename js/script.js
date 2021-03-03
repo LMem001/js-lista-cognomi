48d3dc40ff93a642b0bd31c7d84a9a6b76331ece
Luca <memeoluca6@gmail.com>
2021-03-03 14:45:53 +0100

    first manual commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* Chiedere all’utente il cognome
+inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+stampa la lista ordinata alfabeticamente
+scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova */
+
+// ask user's surname
